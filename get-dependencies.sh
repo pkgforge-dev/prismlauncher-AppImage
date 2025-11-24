68b3f711a30be4033433c16856b72b73e1e52ac5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-24 10:56:49 -0400

    get the thing

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm prismlauncher qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
